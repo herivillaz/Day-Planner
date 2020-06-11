434b3c56b4f7576c88187f700576f8f82b4eb717
herivillaz <herivillaz@gmail.com>
2020-06-11 17:06:17 -0400

    Clean and format script and style

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-// var currentTime = Moment.currentTime()
+    //Create Variable with the Timeblocks/hours.
 $(document).ready(function () {
     var timeInputs = JSON.parse(localStorage.getItem('timeInputs')) || [
         { time: 7, input: "" },
@@ -15,11 +15,10 @@ $(document).ready(function () {
         { time: 18, input: "" },
         { time: 19, input: "" },
     ];
-    console.log(timeInputs);
+
     var currentHour = moment().hours();
     var todaysDate = moment().format("dddd, MMMM Do YYYY");
-    console.log(currentHour);
-    //Create Variable with the hours.
+
 
     //Show Today's date on the DOM
     function printTime() {
@@ -30,12 +29,12 @@ $(document).ready(function () {
     function printInputBlocks() {
         for (let i = 0; i < timeInputs.length; i++) {
             console.log(timeInputs[i].time, timeInputs[i].input);
-            var inputGroup = $('<div class="input-group mb-3" id="times">').css("font-size", "20px");
-            var inputGroupPrepend = $('<div class="input-group-prepend" id="times">');
-            var prependSpan = $('<span class="input-group-text" id="times">' + timeInputs[i].time + ':00' + '</span>').attr({ class: "hour"});
+            var inputGroup = $('<div class="input-group mb-3" >').css("font-size", "20px");
+            var inputGroupPrepend = $('<div class="input-group-prepend" >');
+            var prependSpan = $('<span class="input-group-text" >' + timeInputs[i].time + ':00' + '</span>').attr({ class: "hour"});
             inputGroupPrepend.append(prependSpan);
             var inputEl = $('<input type="text" class="form-control"  value="' + timeInputs[i].input + '" id="times-'+i+'">');
-            var inputGroupAppend = $('<div class="input-group-append" id="times">');
+            var inputGroupAppend = $('<div class="input-group-append" >');
             var appendSpan = $('<span data-time="" class="input-group-text"><button id="saveB-' + i + '">Save</button></span>').attr({ class: "saveBtn"});
             inputGroupAppend.append(appendSpan);
             inputGroup.append(inputGroupPrepend).append(inputEl).append(inputGroupAppend);
@@ -58,7 +57,6 @@ $(document).ready(function () {
                     timeInputs[i].input = toStore;
                 }
             }
-            console.log(timeInputs);
             
             localStorage.setItem("timeInputs",JSON.stringify(timeInputs));
 
@@ -78,119 +76,6 @@ $(document).ready(function () {
     };
   };
 
-
-  //Add Calendar Events to local storage
-// $("#saveB").click(function () {
-//     console.log("button.click")
-//     event.preventDefault();
-//     var currentID = $(this).attr('id');
-//     var toStore = $(("#times-" + currentID)).val();
-//     localStorage.setItem(("myEvent" + currentID), toStore);
-// });
-
-
-
-
-
-
-
-
-
-
-//     function printInputBlocks() {
-//         for (let i = 0; i < timeInputs.length; i++) {
-//             console.log(timeInputs[i].time, timeInputs[i].input);
-//             var inputGroup = $('<div class="input-group mb-3" id="times">');
-//             var inputGroupPrepend = $('<div class="input-group-prepend" id="times">');
-//             var prependSpan = $('<span class="input-group-text" id="times">' + timeInputs[i].time + ':00' + '</span>');
-//             inputGroupPrepend.append(prependSpan);
-//             var inputEl = $('<input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" value="' + timeInputs[i].input + '" id="times">');
-//             var inputGroupAppend = $('<div class="input-group-append" id="times">');
-//             var appendSpan = $('<span data-time="" class="input-group-text" id="times"><button>Save</button></span>');
-//             inputGroupAppend.append(appendSpan);
-//             inputGroup.append(inputGroupPrepend).append(inputEl).append(inputGroupAppend);
-//             $(".container").append(inputGroup);
-//         }
-//     }
-
-
-//     //Check relative time
-// function colorTime() {
-//     for (let i = 0; i < timeInputs.length; i++) {
-//         var calTime = moment().hour(7 + i);
-//         if (moment().isBefore(calTime)) {
-//             $("#times").addClass("future");
-//         } else if (moment().isAfter(calTime)) {
-//             $("#times").addClass("past");
-//         } else {
-//             $("#times").addClass("present");
-//         }
-//     };
-// };
-
-
-
-    // function compareTime() {
-    //     var currentHour = parseInt(moment().format('HH'));
-    //     for (let i = 0; i < timeInputs.length; i++) {
-    //          console.log(currentHour);
-    //     if (currentHour < timeInputs[i].time) {
-    //         $(timeInputs).addClass("past");
-    //     }
-    //     else if (currentHour == timeInputs[i].time) {
-    //         $(timeInputs).css("background", "pink");
-    //     }
-        
-    //     }
-    // }
-    
-    // compareTime();
-
-
-
-    // function compareTime() {
-    //     var nowTime = parseInt(moment().format('HH'));
-
-    //     //Start from 9AM, till 5PM
-    //     for (var i = 0; i >= 23; i++) {
-    //         var timeBlock = parseInt($("#" + nowTime + "hr").attr(".container"));
-    //         console.log(timeBlock);
-
-    //         if (timeBlock < nowTime) {
-    //             $("#" + nowTime + "hr").addClass("past");
-    //         } else if (timeBlock == nowTime) {
-    //             $("#" + nowTime + "hr").addClass("present");
-    //         } else if (timeBlock > nowTime) {
-    //             $("#" + nowTime + "hr").addClass("future");
-    //         }
-    //     }
-    // }
-    //     compareTime();
-
-
-
-    // // Function to compare current time vs timeblock time
-    // // Set color of time block to be past(grey), present (salmon), future(aqua)
-    // function compareTime() {
-    //     var currentTime = parseInt(moment().format('HH'));
-
-    //     //Start from 8AM, til 8PM
-    //     for (time = 8; time < 21; time++) {
-    //         var timeblock = parseInt($("#" + time + "hr").attr("data-index"));
-
-    //         if (timeblock < currentTime) {
-    //             $("#" + time + "hr").css("background", "lightgrey");
-    //         }
-    //         else if (timeblock == currentTime) {
-    //             $("#" + time + "hr").css("background", "salmon");
-    //         }
-    //         else if (timeblock > currentTime) {
-    //             $("#" + time + "hr").css("background", "aqua");
-    //         }
-    //     } // End For 
-    // } // End compareTimesss
-
-    // compareTime();
     printTime();
     printInputBlocks();
     colorTime();
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # Day-Planner
 Work Day Scheduler
+* **The initial page looks like this**
+
+![Home Page](/Assets/ReadMe/home.png)
+
+### You can access here: [Day Planner](https://github.com/herivillaz/HarryPotter-Quiz.git)
+
+This project was made taking in consideration the following requirements in Columbia Coding Boot Camp:
+
+## Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+The following animation demonstrates the application functionality:
+
+![day planner demo](/Assets/05-third-party-apis-homework-demo.gif)
+
+## Built with
+
+* [Moment.js](https://momentjs.com/)
+* [Bootstrap](https://getbootstrap.com/docs/4.4/getting-started/introduction/)
+
+## Features.
+
+Day Planner/Work Scheduler App is a sample application that can be used to manage your day-to-day engagements and work Scheduler.
+
+* The current day is displayed at the top of the calendar, and it changes everyday.
+
+![Current Time](/Assets/ReadMe/currentday.png)
+
+* The Timeblock is color coded to indicate whether it is in the past, present, or future:
+* Gray for Past,
+* Pink or Salmon for current time,
+* Green for future.
+
+![Timeblocks](/Assets/ReadMe/hours.png)
+
+* A space input is provided to the user, in that way the user can introduce events, meetings on the time desired.
+
+![Timeblocks](/Assets/ReadMe/localstorage.png)
+
+* When the user clicks the Button SAVE the event, whatever he typed in the text area keeps in local storage, even if he refresh the page.
+
+* No project comes without its own set of difficulties and issues. There were a few things I had issues with along the way. It took me some time to be sure I stepped on each element. To Apply the local storage was the most challenging part.
+* Hope you enjoy my project. Click and Trust my Day Planner Scheduler.
+* I would appreciate your feedback to make this better. Thank You.
